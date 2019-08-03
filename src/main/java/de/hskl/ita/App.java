5effba19d41ae1bfb1927fcb3924e3220446c1c2
Manfred Brill <manfred.brill@hs-kl.de>
2019-08-03 12:32:55 +0200

    Externals in einem Maven-Projekt
    pom.xml ist um maven-assembly-plugin erweitert worden. In den
    dependencies ist logj42 eingetragen.
    
    In App.java gibt es Logging-Aufrufe, und in den resources gibt es die
    Konfiguration log4j.xml.
    
    Mit mvn package wird ein jar-File mit externals erstellt, der direkt mit
    java -jar ausgeführt werden kann. Aktuell ist ein File-Logger
    konfiguriert, die Ausgaben liegen in target/logs.

--- src/main/java/de/hskl/ita/App.java
+++ src/main/java/de/hskl/ita/App.java
@@ -1,14 +1,26 @@
 package de.hskl.ita;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
- * Hello world!
+ * Hello world.
  *
+ * Diese Klasse wurde von Maven angelegt
+ * und um eine weitere Ausgabe auf der Konsole erweitert.
+ *
+ * In dieser Version fügen wir Logging-Ausgaben
+ * mit Hilfe von Log4j2 ein, um zu demonstrieren,
+ * wie wir externals mit Maven auflösen.
  */
 public class App 
 {
+    private static final Logger log=LogManager.getLogger();
+
     public static void main( String[] args )
     {
+	log.info(">> App.main");
         System.out.println( "Hello World!" );
 	System.out.println( "My first Maven projekt" );
+	log.info("<< App.main");
     }
 }
