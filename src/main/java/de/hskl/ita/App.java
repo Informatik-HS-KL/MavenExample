f98d969641c08e75c71e0c2fbf3a9c613740b189
Manfred Brill <manfred.brill@hs-kl.de>
2019-08-03 11:49:10 +0200

    Erstes Commit

---
+++ src/main/java/de/hskl/ita/App.java
@@ -0,0 +1,14 @@
+package de.hskl.ita;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println( "My first Maven projekt" );
+    }
+}
