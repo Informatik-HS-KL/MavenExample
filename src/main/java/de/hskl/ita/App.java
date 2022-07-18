dba2d552d941cf5434939de4dee8cb95e3cad7b5
MBrill <manfred.brill@hskl.de>
2022-07-18 12:05:57 +0200

    Test in Eclipse 2022-6 und auf Konsole
    mvn compile und mvn test laufen auf evans durch.
    
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    modified:   src/main/java/de/hskl/ita/App.java

--- src/main/java/de/hskl/ita/App.java
+++ src/main/java/de/hskl/ita/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "My first Maven projekt" );
+	    System.out.println( "My first Maven projekt" );
     }
 }
